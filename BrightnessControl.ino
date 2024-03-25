bf9e8b77c77ca8ae111adc922c860ec376e7f0c3
Azaken1248 <suvkrm@gmail.com>
2024-03-26 00:59:05 +0530

    Adding Brightness Control

---
+++ BrightnessControl.ino
@@ -0,0 +1,25 @@
+const int ledPin = 2;
+
+void setup()
+{
+    pinMode(ledPin, OUTPUT);
+}
+
+void loop()
+{
+    for (int brightness = 0; brightness <= 255; brightness++)
+    {
+        analogWrite(ledPin, brightness);
+        delay(10);
+    }
+
+    delay(1000);
+
+    for (int brightness = 255; brightness >= 0; brightness--)
+    {
+        analogWrite(ledPin, brightness);
+        delay(10);
+    }
+
+    delay(1000);
+}
